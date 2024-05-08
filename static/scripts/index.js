c0f72f9715750e68a170b35079bac5827d2945a1
z0hnan <simesim2006@gmail.com>
2024-05-08 15:42:03 +0200

    uhhhh animation hehehaw

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Make a GET request to fetch data
-    fetch('/get_data')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data); // Log retrieved data
-            // Optionally, update UI or perform other actions with the data
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Optionally, handle error
-        });
+    $('.grid-img').hover(
+        function() {
+          $(this).siblings('.grid-text').css('opacity', 1);
+          $(this).css("filter", "blur(5px)"); // Separate multiple CSS properties correctly
+          $(this).css("-webkit-filter", "blur(5px)");
+          $(this).css("transform", "scale(1.1)");        
+        },
+        function() {
+          $(this).siblings('.grid-text').css('opacity', 0);
+          $(this).css("filter", "blur(0px)");
+          $(this).css("-webkit-filter", "blur(0px)");
+          $(this).css("transform", "scale(1)");      
+        }
+      );
+      $('.grid-text').hover(
+        function() {
+          $(this).css('opacity', 1);
+          $(this).siblings('.grid-img').css("filter", "blur(5px)");
+          $(this).siblings('.grid-img').css("-webkit-filter", "blur(5px)");
+          $(this).siblings('.grid-img').css("transform", "scale(1.1)");        
+        },
+        function() {
+          $(this).css('opacity', 0);
+          $(this).siblings('.grid-img').css("filter", "blur(0px)");
+          $(this).siblings('.grid-img').css("-webkit-filter", "blur(0px)");
+          $(this).siblings('.grid-img').css("transform", "scale(1)");        
+        }
+      );
 });
