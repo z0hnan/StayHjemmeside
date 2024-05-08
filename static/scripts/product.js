bd9820804bf6e6b49e21b155781ac3e83b1130a3
Jakob Haaning Andersen <jako803a@gmail.com>
2024-05-08 14:51:48 +0200

    johnhhohesi0da

--- static/scripts/product.js
+++ static/scripts/product.js
@@ -1,4 +1,33 @@
-const itemID = 1;
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all links with class "link"
+    var links = document.querySelectorAll('.grid-img');
+
+    // Loop through each link
+    links.forEach(function(link) {
+        // Attach a click event listener to each link
+        link.addEventListener('click', function(event) {
+            // Prevent the default link behavior
+            event.preventDefault();
+
+            // Get the href attribute of the clicked link
+            var href = this.getAttribute('href');
+
+            // Extract variables from the href
+            var urlParams = new URLSearchParams(href.split('?')[1]);
+            var var1 = urlParams.get('var1');
+
+            // Pass variables to the JavaScript file
+            getItemid(var1);
+        });
+    });
+});
+
+function getItemid(var1) {
+    // Do something with the variables
+    console.log('Variable 1:', var1);
+    var itemID = var1;
+    
+}
 
 function AddToCart(productId, productName, productPrice) {
     alert("Product added to cart");
