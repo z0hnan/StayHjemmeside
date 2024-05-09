dfad68e075fc5ab062160ebace1d1e79332be779
Jakob Haaning Andersen <jako803a@gmail.com>
2024-05-09 14:40:15 +0200

    Lidt formatering af koden så det står rigtigt

--- static/scripts/product.js
+++ static/scripts/product.js
@@ -1,13 +1,13 @@
 var itemID = sessionStorage.getItem('itemID');
 console.log('Item ID:', itemID);
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Make a GET request to fetch data
     fetch('/get_data_item')
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            console.log(itemID);            
+            console.log(itemID);
             const item = data[itemID - 1];
             const title = item[1];
             const price = item[2];
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const priceElement = document.getElementById('priceElement');
             priceElement.textContent = price;
             const imgElement = document.getElementById('imgElement');
-            imgElement.style.backgroundImage ="url(" + img+")";
+            imgElement.style.backgroundImage = "url(" + img + ")";
             const descriptionElement = document.getElementById('descriptionElement');
             descriptionElement.textContent = description;
         })
